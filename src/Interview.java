bf902a12f6a9a76b849e5e4f2709a09c41764bfb
Franck Tiomo <77629271+FranckTiomo@users.noreply.github.com>
2021-11-26 21:19:55 -0500

    Revert "Update Interview.java"
    This reverts commit e34ee46f999df5bac5ad193b290ec244fbd43dc5.

--- src/Interview.java
+++ src/Interview.java
@@ -1,26 +1,15 @@
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Interview {
-        // La compléxité est de O(n), on itère sur tout les éléments de heap, avec while(!heap.isEmpty()) en O(n),
-        // les autre opérations dans la méthode sont en O(1)
+
         public int lastBox(int[] boxes){
 
             // Ne pas modifier la ligne suivante
             PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
-            Arrays.stream(boxes).forEach(heap::add);
 
-            while(!heap.isEmpty()) {
-                if (heap.size() == 1) return heap.poll();
-
-                int box1 = heap.poll();
-                int box2 = heap.poll();
-
-                if (box1 != box2) {
-                    heap.add(Math.abs(box1 - box2));
-                }
-            }
-            return 0;
+            return -1;
         }
 }
+
+
